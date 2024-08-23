eecbdf2c22d8c503a685f0eb0462ad378f653834
thanaphon.chaiyanit <b6220655@g.sut.ac.th>
2024-08-23 21:24:01 +0700

    start my project

--- myself/src/App.jsx
+++ myself/src/App.jsx
@@ -1,37 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import myImage from '../picture/StockCake-Creative Desktop Setup_1723891320.jpg';
 
-
+const background = {
+  backgroundColor: 'lightblue', // สีพื้นหลัง
+  height: '100vh', // ความสูงเต็มหน้าจอ
+  width: 'auto',
+  backgroundSize: 'cover',
+  display: 'flex', // การจัดวางแนวตั้ง
+  //justifyContent: 'center', // จัดแนวแนวนอน
+  //alignItems: 'center' // จัดแนวแนวนอน
+};
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <div className="background">
-      <div>
-      <h1>Welcome to My Website</h1>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div style={background}>
+      <AppBar 
+        position='fixed'>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      <img src={myImage} alt="Description of image" style={{margin: '60px 0 0 15px', width: '200px', height: '200px', border: '2px solid black' }} />
+      <div style={{margin: '60px 0 0 40px'}}>
+        <h2>
+          CAREER
+        </h2>
+        <span>
+        I am a new graduate interested in the position of Junior Full Stack Developer, able to coordinate with others. and have high computer skills Where I can apply my skills to your team success.
+        </span>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
       </div>
     </>
   )
