48c3fbe5f9954c262bfaeab9f209f2bed940d92f
thanaphon.chaiyanit <b6220655@g.sut.ac.th>
2024-08-26 13:15:10 +0700

    เพิ่มวิดีโอ

--- myself/src/component/Experience.jsx
+++ myself/src/component/Experience.jsx
@@ -1,6 +1,8 @@
 import Box from '@mui/material/Box';
+import ReactPlayer from 'react-player';
 
 function Experience () {
+    const par = { margin: '20px 0 10px 60px' };
     return (
         <>
         <div style={{margin: '50px 0 0 15px', display: 'flex', justifyContent: 'center'}}>
@@ -28,46 +30,103 @@ function Experience () {
             <div style={{margin: '0px 0 0 20px'}}>
                 <span>
                     <h2>
-                            2024 : IT Application Support Officer
+                    2024 : IT Application Support Officer <small style={{ fontSize: '18px' }}>(18 March - 31 July)</small>
                     </h2>
                 </span>
+                <strong style={{ fontSize: '18px', margin: '0 0 0 20px'}}>KIG : KKF international groups co. ltd</strong>
                 <div style={{display: 'flex', alignItems: 'center'}}>
                     <img 
                         src="https://kigintergroup.com/public/themes/kig/assets/images/home/logo.png"
-                        alt="SUT Logo"
+                        alt="KIG Logo"
                         style={{
                             width: '250px',
                             height: 'auto',
-                            margin: '0 10px 0 40px',
+                            margin: '5px 10px 0 40px',
                             }}
                     />
                 </div>
                 <span>
-                    <h3 style={{margin: '20px 0 10px 20px'}}>
+                    <h3 style={{margin: '20px 0 10px 40px'}}>
                         หน้าที่รับผิดชอบ
                     </h3>
-                    <div style={{margin: '0 0 0 45px'}}>
+                    <div style={par}>
                         1.ตรวจสอบโปรแกรมที่โปรแกรมเมอร์นำมาติดตั้งให้ได้ตามมาตรฐาน และสามารถสรุปผลการตรวจสอบให้โปรแกรมเมอร์ 
                         เข้าใจถึงปัญหา และนำไปตรวจสอบแก้ไขได้ถูกต้อง ก่อนติดตั้งให้ลูกค้าได้ใช้งานจริง
                     </div>
-                    <div style={{margin: '0 0 0 45px'}}>
+                    <div style={par}>
                         2. พัฒนาระบบโปรแกรมคอมพิวเตอร์ โดยศึกษาทำความเข้าใจ Program Specification และข้อมูลอื่นๆ ที่เกี่ยวข้อง 
                         และดำเนินการเขียนโปรแกรมให้ได้ตามคุณสมบัติที่กำหนดไว้
                     </div>
-                    <div style={{margin: '0 0 0 45px'}}>
+                    <div style={par}>
                         3. ติดตั้งระบบโปรแกรมและอบรมวิธีการใช้งานโปรแกรมให้ลูกค้าหน่วยงานต่างๆ เข้าใจและสามารถใช้งานโปรแกรมได้
                     </div>
-                    <div style={{margin: '0 0 0 45px'}}>
+                    <div style={par}>
                         4. จัดทำเอกสารที่เกี่ยวข้องกับการตรวจสอบระบบโปรแกรม ตลอดจนเอกสารที่ใช้ในการส่งมอบติดตั้งระบบ ได้ถูกต้องครบถ้วน
                     </div>
-                    <div style={{margin: '0 0 0 45px'}}>
+                    <div style={par}>
                         5. ดูแลระบบโปรแกรมคอมพิวเตอร์ เพื่อสามารถใช้งานได้อย่างมีประสิทธิภาพและเป็นไปตามมาตรฐานระบบโปรแกรมของบริษัท
                     </div>
                 </span>
             </div>
             </Box>
         </div>
-        <div style={{display: 'flex', justifyContent: 'center'}}>      
+        <div style={{display: 'flex', margin: '50px 0 0 30px'}}>  
+        <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
+            <div style={{margin: '0px 0 0 20px'}}>
+                <span>
+                    <h2>
+                    2023 : Software Engineer <small style={{ fontSize: '18px' }}>(Cooperative Education 17 July - 3 November)</small>
+                    </h2>
+                </span>
+                <strong style={{ fontSize: '18px', margin: '0 0 0 20px'}}>BANGKOK SMARTCARD SYSTEM COMPANY LIMITED</strong>
+                <div style={{display: 'flex', alignItems: 'center'}}>
+                    <img 
+                        src="https://www.rabbit.co.th/images/rabbit-logo.svg"
+                        alt="BSS Logo"
+                        style={{
+                            width: '250px',
+                            height: 'auto',
+                            margin: '5px 10px 0 40px',
+                            }}
+                    />
+                </div>
+                <span>
+                    <h3 style={{margin: '20px 0 10px 40px'}}>
+                        หน้าที่รับผิดชอบ
+                    </h3>
+                    <div style={par}>
+                        1.สร้างโปรเจ็คตาม Requirement
+                    </div>
+                    <div style={par}>
+                    Full Stack WebDev Pros Cons<br />
+                    - Next.js (React)<br />
+                    - SvelteKit (Svelte)<br /><br />
+
+                    Requirement<br />
+                    - One UI Component (Can use 3rd-party dependencies but same UI Component)<br />
+                    - Firebase AppCheck<br />
+                    - Firebase Authentication (Google Sign-in + Email, Password)<br />
+                    - Server Side Rendering by Allowed Permission<br />
+                    - API authorized with Firebase Access Token or Session and AppCheck Token<br />
+                    - API connect to Database<br />
+                    - Deploy Docker<br /><br />
+
+                    UI Components<br />
+                    - Date Picker (Must)<br />
+                    - Time Picker (Optional)<br />
+                    - Modal<br />
+                    - Form<br />
+                    - Grid<br />
+                    - Side Navigation Menu<br />
+                    - Desktop Friendly (Must)<br />
+                    - Mobile Friendly<br />
+                    - Tailwind CSS maybe good<br />
+                    - Free & Open-source & See License<br />
+                    </div>
+                    <ReactPlayer url='https://youtu.be/mLR659WgU4w' width='1367px' />
+                </span>
+            </div>
+            </Box>
         </div>
         </>
     )  
